89c9758350680cf11baa6bf1be2ea399051196ec
Pedro Barbiero <pedro.barbiero@db1.com.br>
2022-05-13 12:41:43 -0300

    example 3 packages

---
+++ client/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/barbierodb1/workmodexp/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.Dial(
+		"localhost:55051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := proto.NewTodoServiceClient(conn)
+
+	runSomething(client)
+}
+
+func runSomething(client proto.TodoServiceClient) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	log.Print("running a Create and then a Get")
+
+	todo := &proto.Todo{Task: "This is a task"}
+	res, err := client.Create(ctx, todo)
+	if err != nil {
+		log.Print("ERR Creating:", err)
+		return
+	}
+	if res.Task != todo.Task {
+		log.Printf("WARN: res.Task == %s but todo.Task == %s", res.Task, todo.Task)
+	}
+
+	gotten, err := client.Get(ctx, &proto.Todo{Id: res.GetId()})
+	if err != nil {
+		log.Print("ERR Getting: ", err)
+		return
+	}
+	if gotten.Task != todo.Task {
+		log.Printf("WARN: gotten.Task == %s but todo.Task == %s", gotten.Task, todo.Task)
+	}
+
+	log.Print("Finished running Create and then Get")
+}
---
+++ service/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"github.com/barbierodb1/workmodexp/proto"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type todoService struct {
+	proto.UnimplementedTodoServiceServer
+
+	todos map[uuid.UUID]*proto.Todo
+}
+
+func (t *todoService) Create(ctx context.Context, todo *proto.Todo) (*proto.Todo, error) {
+	if t.todos == nil {
+		t.todos = map[uuid.UUID]*proto.Todo{}
+	}
+
+	if todo.GetId() != "" {
+		return nil, status.Error(codes.InvalidArgument, "ID must not be set for creation")
+	}
+
+	id := uuid.New()
+	newTodo := &proto.Todo{
+		Id:        id.String(),
+		Task:      todo.Task,
+		CreatedAt: timestamppb.Now(),
+	}
+	t.todos[id] = newTodo
+
+	return newTodo, status.FromContextError(ctx.Err()).Err()
+}
+
+func (t *todoService) Get(ctx context.Context, todo *proto.Todo) (*proto.Todo, error) {
+	if t.todos == nil {
+		t.todos = map[uuid.UUID]*proto.Todo{}
+	}
+
+	if todo.GetId() == "" {
+		return nil, status.Error(codes.InvalidArgument, "ID must be set for Get")
+	}
+
+	parsedID, err := uuid.Parse(todo.GetId())
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid uuid. err: %s", err.Error())
+	}
+
+	foundTodo, ok := t.todos[parsedID]
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, "id %s not found", err.Error())
+	}
+
+	return foundTodo, status.FromContextError(ctx.Err()).Err()
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:55051")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+
+	server := grpc.NewServer(
+		grpc.Creds(insecure.NewCredentials()),
+	)
+	proto.RegisterTodoServiceServer(server, &todoService{})
+	defer server.Stop()
+
+	go func(lis net.Listener, server *grpc.Server) {
+		log.Printf("Running server at address %s...", lis.Addr())
+		if err := server.Serve(lis); err != nil {
+			log.Print(err)
+		}
+	}(lis, server)
+
+	<-sigs
+	log.Print("Closing server...")
+	server.GracefulStop()
+}
