3e628aca7b0e44eef5958b8eac10ee0260d6c95d
Pedro Barbiero <pedro.barbiero@db1.com.br>
2022-05-16 16:30:07 -0300

    better support deep modules

--- deep/modul/exported_type.go
+++ deep/modul/exported_type.go
@@ -1,7 +1,8 @@
 package modul
 
 type ExportedType struct {
-	ExportedField string
-	Number        int
-	Decimal       float64
+	ExportedField  string
+	Number         int
+	Decimal        float64
+	SomeOtherField string
 }
