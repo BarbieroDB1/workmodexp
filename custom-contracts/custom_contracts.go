f772bbc22fc6857a3ebc300a14fbc62ba97a2beb
Pedro Barbiero <pedro.barbiero@db1.com.br>
2022-05-17 11:24:47 -0300

    less fields + debug

--- custom-contracts/custom_contracts.go
+++ custom-contracts/custom_contracts.go
@@ -1,17 +1,14 @@
 package customcontracts
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 )
 
 type CustomContracts struct {
-	Name      string
-	ID        uuid.UUID
-	CreatedAt time.Time
+	Name string
+	ID   uuid.UUID
 }
 
 func NewCustomContracts(name string) CustomContracts {
-	return CustomContracts{Name: name, ID: uuid.New(), CreatedAt: time.Now()}
+	return CustomContracts{Name: name, ID: uuid.New()}
 }
